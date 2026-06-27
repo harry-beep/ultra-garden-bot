f2312f5f604987f323e7ce9fed7dcb4e188ffd1e
harry-beep <68366888+harry-beep@users.noreply.github.com>
2026-06-27 23:14:12 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import aiosqlite
 import random
 import datetime
 
-TOKEN = "YOUR_BOT_TOKEN"
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
